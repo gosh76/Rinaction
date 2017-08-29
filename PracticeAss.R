7a85e53915fc0c5c762b751ad7e1e2cf47679624
gosh76 <goldiesahni@gmail.com>
2017-08-29 13:59:28 +0530

    29082017

--- PracticeAss.R
+++ PracticeAss.R
@@ -4,6 +4,7 @@ x1 = rep(x,c(3,5,2,4,4))
 x1
 x2 = c(x1[1:10],c(10,10),x1[11:18])
 x2
+mean(x2)
 table(x2)
 # A2
 marks = c(10,12,13,14,17)
@@ -13,7 +14,28 @@ DFD = marks*students
 DFD
 DFDm = sum(DFD)/sum(students)
 DFDm
-marks1 = c(15,25,35,45)
+x = rep(marks,students)
+x
+mean(x)
+table(x)
+marks2c = c('10-20','20-30','30-40','40-50')
+students2c = c(5,7,9,8)
+df2c = data.frame(marks2c,students2c)
+df2c
+str(df2c)
+midpt2c = c(15,25,35,45)
+df2c$mid2c = midpt2c
+df2c
+df2c$total = df2c$students2c*df2c$mid2c
+df2c
+sum(df2c$total)/sum(df2c$students2c)
+x = rep(df2c$mid2c,df2c$students2c)
+mean(x)
+x3a = rep(marks,students)
+x3a
+hist(x3a)
+
+xmarks1 = c(15,25,35,45)
 students1 = c(5,7,8,9)
 CFD = marks1*students1
 CFD
@@ -30,3 +52,5 @@ m.cut
 m.table = table(m.cut)
 m.table
 cbind(m.table)
+m1 = matrix(marks,students)
+hist(m1)
