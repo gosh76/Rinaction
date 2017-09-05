f6c87e19ecba52fc77daf8374dddad460274407f
gosh76 <goldiesahni@gmail.com>
2017-09-05 19:02:22 +0530

    05092017

--- Assignment-2.R
+++ Assignment-2.R
@@ -1,11 +1,12 @@
 df1 <- read.csv('./data/dsstudents.csv')# data read in form of dataframe df1
-df1
 v1 <- df1$excel # df1 column excel assigned to a vector v1
 names(v1) <- df1$name # df1 column name assigned to names for vector v1
-v1
+par('mar')
+par(mar=c(7,7,7,7))
 # displaying barplot of students names and their excel marks in vertical bars
-barplot(v1,xlab="Names", ylab="Marks", main="Excel Marks",las=2,col=c(1,2,3,4,5,6,7,8),horiz=F,ylim=c(0,100),cex.names=0.6)
+barplot(v1,ylab="Marks", main="Excel Marks",las=2,col=c(1,2,3,4,5,6,7,8),horiz=F,ylim=c(0,100),cex.names=0.6)
+par(mar=c(5,8,5,5))
 # displaying barplot of students names and their excel marks in horizontal bars
-barplot(v1,xlab="Marks", ylab="Names", main="Excel Marks",las=2,col=c(1,2,3,4,5,6,7,8),horiz=T,xlim=c(0,100),cex.names=0.6)
+barplot(v1,xlab="Marks",main="Excel Marks",las=2,col=c(1,2,3,4,5,6,7,8),horiz=T,xlim=c(0,100),cex.names=0.6)
 
 
