e26bd996920f8e4a010aae16ae3163e9d5d5b439
gosh76 <goldiesahni@gmail.com>
2017-10-15 13:56:15 +0530

    15102017

--- data/Ass6.R
+++ data/Ass6.R
@@ -1,7 +1,29 @@
-seq(10,200,length.out=40)
-seq.int(10,200,length.out = 40)
-M = matrix(1:12,ncol=3)
+#Creating a vector of 40 integer values
+v1 <- ceiling(seq(10,200,length.out=40))
+v1
+#Creating a 5X8 matrix
+M = matrix(v1,ncol=8)
 M
+#return product of each of rows
+apply(M,1,prod)
+#return sum of each of columns
+apply(M,2,sum)
+# modulo of matrix values
+M1 = sweep(M,1,10,FUN='%%')
+M1
+# odd nos.in each column
+M2 = sweep(M1,1,2,FUN='%%')
+M2
+apply(M2,2,sum)
+# viewing iris dataset
+i = iris
+i
+# mean of first 4 variables specieswise
+agg <- aggregate(i,by=list(i$Species),FUN=mean,na.rm=TRUE)
+agg
+
+
+
 M+1
 M+c(1,2)
 M+c(1,2,3)
