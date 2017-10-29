d71bfa0abb935c0ac0b55cad4a6d39206c56145e
gosh76 <goldiesahni@gmail.com>
2017-10-29 14:24:29 +0530

    29102017

---
+++ mode.R
@@ -0,0 +1,10 @@
+x <- c(1,5,7,8,5)
+table(x)
+sort(table(x))
+names(sort(table(x),decreasing=T))[1]
+x1 = c(1:12,1,5,5)
+table(x1)
+sort(table(x1),decreasing=T)
+names(table(x1))
+sort(table(x1),decr=T)[1]
+names(sort(table(x1),decr=T))[1]
