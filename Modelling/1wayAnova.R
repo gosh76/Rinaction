ec5c78be9a96b07439d1d97fa0a1263d51356829
gosh76 <goldiesahni@gmail.com>
2017-11-15 13:04:50 +0530

    15112017

--- Modelling/1wayAnova.R
+++ Modelling/1wayAnova.R
@@ -5,4 +5,26 @@ S4 <- c(25.4,19.9,22.6,17.5,20.4)
 df <- data.frame(S1,S2,S3,S4)
 df
 colMeans(df)
-addmargins(df,c(1,2),FUN=list(list(mean,sum,length),list(length)))
+addmargins(as.matrix(df),c(1,2),FUN=list(list(mean,sd,length),list(length)))
+df
+dfstack = stack(df)
+str(dfstack)
+aggregate(dfstack$values,by=list(dfstack$ind),FUN=mean)
+aggregate(dfstack$values,by=list(dfstack$ind),FUN=sd)
+a1 = aov(values~ind,data=dfstack)
+summary(a1)
+qf(p=0.95,df1=3,df2=16)# for F-critical
+boxplot(dfstack$values)
+boxplot(df)
+boxplot(values~ind,data=dfstack)
+gplots::plotmeans(values~ind,data=dfstack)
+abline(h=19.52)
+abline(h=colMeans(df))#means are not same
+TukeyHSD(a1)
+plot(TukeyHSD(a1))# S2 - S1 is largest
+library(multcomp)
+tuk= glht(a1,linfct=mcp(ind="Tukey"))
+plot(cld(tuk,level=0.05))
+car::qqPlot(a1,simulate=T,labels=F)#check normality of dependent variable
+bartlett.test(values~ind,data = dfstack)
+car::outlierTest(a1)
